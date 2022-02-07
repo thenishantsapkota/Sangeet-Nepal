a11d5b78d3fc91fa3bb3b6c1b6f97daeaff04650
Nishant Sapkota <snishant306@gmail.com>
2022-02-07 19:55:31 +0545

    feat: add `play` and `queue` command

--- sangeet_nepal/core/plugins/music.py
+++ sangeet_nepal/core/plugins/music.py
@@ -1,28 +1,164 @@
-import lightbulb
+from datetime import datetime
 
-from . import join, leave
+import hikari
+import lavasnek_rs
+import lightbulb
+import miru
+from miru.ext import nav
+
+from . import _chunk, fetch_lavalink, join, leave
 
 music = lightbulb.Plugin("Music", "Music Commands")
 
 
+class MusicError(lightbulb.LightbulbError):
+    pass
+
+
 @music.command
-@lightbulb.command("join", "Join the voice channel")
+@lightbulb.command("join", "Make the bot join the voice channel")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def join_command(ctx: lightbulb.Context) -> None:
     channel_id = await join(ctx)
     if channel_id:
-        await ctx.respond("Joined <#{}>".format(channel_id))
+        await ctx.respond(
+            embed=hikari.Embed(
+                description="Joined <#{}>".format(channel_id), color=0x00FF00
+            )
+        )
 
 
 @music.command
-@lightbulb.command("leave", "Leave the voice channel")
+@lightbulb.command(
+    "leave",
+    "Make the bot leave the voice channel",
+)
 @lightbulb.implements(lightbulb.SlashCommand)
 async def leave_command(ctx: lightbulb.Context) -> None:
     await leave(ctx)
+    await ctx.respond(
+        embed=hikari.Embed(description="Disconnected from voice!", color=0x00FF00)
+    )
+
+
+@music.command
+@lightbulb.option("query", "Query for the play command[Name or URL of the song]")
+@lightbulb.command("play", "Play some music using the bot")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def play_command(ctx: lightbulb.Context) -> None:
+    lavalink = fetch_lavalink(ctx.bot)
+    query = ctx.options.query
+    connection_info = lavalink.get_guild_gateway_connection_info(ctx.guild_id)
+    if not connection_info:
+        await join(ctx)
+
+    query_information = await lavalink.auto_search_tracks(query)
+    playlist = False
+    if query_information.playlist_info.name:
+        playlist = True
+
+    if not query_information.tracks:
+        raise MusicError("No matching video to the given query.")
+
+    if playlist:
+        try:
+            for track in query_information.tracks:
+                await lavalink.play(ctx.guild_id, track).requester(
+                    ctx.author.id
+                ).queue()
+        except lavasnek_rs.NoSessionPresent:
+            raise MusicError("I am not connected to any voice channel!")
+
+        await ctx.respond(
+            embed=hikari.Embed(
+                title="Playlist Added",
+                description=f"{query_information.playlist_info.name}({len(query_information.tracks)} tracks added to queue [{ctx.author.mention}])",
+                color=0x00FF00,
+            )
+        )
+    else:
+        try:
+            await lavalink.play(ctx.guild_id, query_information.tracks[0]).requester(
+                ctx.author.id
+            ).queue()
+        except lavasnek_rs.NoSessionPresent:
+            raise MusicError("I am not connected to any voice channel")
+
+        await ctx.respond(
+            embed=hikari.Embed(
+                title="Track Added",
+                description=f"[{query_information.tracks[0].info.title}]({query_information.tracks[0].info.uri}) added to the queue [{ctx.author.mention}]",
+                color=0x00FF00,
+            )
+        )
+
+
+@music.command
+@lightbulb.command("clear", "Clear the current queue")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def clear_command(ctx: lightbulb.Context) -> None:
+    await leave(ctx)
+    await join(ctx)
+    await ctx.respond(
+        embed=hikari.Embed(description="Cleared the current queue!", color=0x00FF00)
+    )
+
+
+@music.command
+@lightbulb.command("queue", "Displays the current music queue for the server")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def queue_command(ctx: lightbulb.Context) -> None:
+    lavalink = fetch_lavalink(ctx.bot)
+    song_queue = []
+    node = await lavalink.get_guild_node(ctx.guild_id)
+    if not node or not node.queue:
+        raise MusicError("There are no tracks in the queue.")
+
+    for song in node.queue:
+        song_queue += [
+            f"[{song.track.info.title}]({song.track.info.uri}) [<@{song.requester}>]"
+        ]
+
+    fields = []
+    counter = 1
+    if not len(song_queue[1:]) > 0:
+        return await ctx.respond(
+            f"No tracks in the queue.\n**Now Playing** : [{node.now_playing.track.info.title}]({node.now_playing.track.info.uri})"
+        )
+    for index, track in enumerate(_chunk(song_queue[1:], 8)):
+        string = """**Next Up**\n"""
+        for i in track:
+            amount = node.queue[counter].track.info.length
+            millis = int(amount)
+            l_seconds = (millis / 1000) % 60
+            l_seconds = int(l_seconds)
+            l_minutes = (millis / (1000 * 60)) % 60
+            l_minutes = int(l_minutes)
+            first_n = int(l_seconds / 10)
+            string += f"""{counter}.`{l_minutes}:{l_seconds if first_n !=0 else f'0{l_seconds}'}` {i}\n"""
+
+            counter += 1
+        embed = hikari.Embed(
+            title=f"Queue for {ctx.get_guild()}",
+            color=0x00FF00,
+            timestamp=datetime.now().astimezone(),
+            description=string,
+        )
+        embed.set_footer(text=f"Page {index+1}")
+        embed.add_field(
+            name="Now Playing",
+            value=f"[{node.now_playing.track.info.title}]({node.now_playing.track.info.uri}) [<@{node.now_playing.requester}>]",  # noqa: E501
+        )
+        fields.append(embed)
+
+    navigator = nav.NavigatorView(pages=fields)
+    await navigator.send(ctx.interaction)
+    await navigator.wait()
 
 
 def load(bot: lightbulb.BotApp) -> None:
     bot.add_plugin(music)
+    miru.load(bot)
 
 
 def unload(bot: lightbulb.BotApp) -> None:
