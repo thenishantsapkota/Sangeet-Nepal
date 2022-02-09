7e4b8ee517d10cba516e29691ac2f90e41825e2f
Nishant Sapkota <snishant306@gmail.com>
2022-02-09 10:59:20 +0545

    feat: add config file for `Spotify`

--- sangeet_nepal/config/__init__.py
+++ sangeet_nepal/config/__init__.py
@@ -1,5 +1,6 @@
 from .bot import bot_config
 from .database import db_config
 from .lavalink import lavalink_config
+from .spotify import spotify_config
 
-__all__ = ["bot_config", "db_config", "lavalink_config"]
+__all__ = ["bot_config", "db_config", "lavalink_config", "spotify_config"]
---
+++ sangeet_nepal/config/spotify.py
@@ -0,0 +1,15 @@
+from lib2to3.pytree import Base
+
+from pydantic import BaseSettings
+
+
+class SpotifyConfig(BaseSettings):
+    client_id: str
+    client_secret: str
+
+    class Config:
+        env_file = ".env"
+        env_prefix = "spotify_"
+
+
+spotify_config = SpotifyConfig()
