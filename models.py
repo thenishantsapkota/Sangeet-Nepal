9c818a4e8ec054da0e80dac6c525a44b54c9dbc8
Nishant Sapkota <snishant306@gmail.com>
2022-03-23 18:44:34 +0545

    feat: add `SavedPlaylists` model to store saved playlists

---
+++ models.py
@@ -0,0 +1,14 @@
+from tortoise import fields
+from tortoise.models import Model
+
+
+class SavedPlaylists(Model):
+    id = fields.IntField(pk=True)
+    guild_id = fields.BigIntField()
+    member_id = fields.BigIntField()
+    playlist_url = fields.TextField()
+    playlist_name = fields.TextField()
+
+    class Meta:
+        table = "saved_playlists"
+        table_description = "This table stores saved user playlists"
