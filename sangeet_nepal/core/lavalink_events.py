21b42736758e60ad8e9b1755f85f04112e2a5ce9
Nishant Sapkota <snishant306@gmail.com>
2022-11-19 09:53:57 +0545

    chore(deps): update all the dependencies

--- sangeet_nepal/core/lavalink_events.py
+++ sangeet_nepal/core/lavalink_events.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING
 import hikari
 import lavasnek_rs
 from lightbulb import LightbulbError
+
 from sangeet_nepal.config.bot import bot_config
 
 if TYPE_CHECKING:
@@ -57,21 +58,6 @@ class LavalinkEvents:
         self, lavalink: lavasnek_rs.Lavalink, event: lavasnek_rs.TrackFinish
     ) -> None:
         guild = self.bot.cache.get_guild(event.guild_id)
-
-        states = self.bot.cache.get_voice_states_view_for_guild(event.guild_id)
-
-        members = [
-            state
-            async for state in states.iterator().filter(
-                lambda i: i.user_id != self.bot.application.id
-            )
-        ]
-        if len(members) == 0:
-            await lavalink.destroy(event.guild_id)
-            await lavalink.stop(event.guild_id)
-            await lavalink.leave(event.guild_id)
-            await lavalink.remove_guild_node(event.guild_id)
-            await lavalink.remove_guild_from_loops(event.guild_id)
 
         logger.info("Track finished on guild - {}".format(event.guild_id))
         await self.bot.rest.create_message(
